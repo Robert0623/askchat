c6c92a9e298936ea5ad1179fdc26160909f04ed4
hwoo <tone8943@gmail.com>
2025-03-22 18:05:18 +0900

    ChatModel로 응답 받기 - 1

--- src/main/java/com/example/askchat/controller/AskAIController.java
+++
@@ -1,4 +0,0 @@
-package com.example.askchat.controller;
-
-public class AskAIController {
-}
--- src/main/java/com/example/askchat/service/ChatService.java
+++ src/main/java/com/example/askchat/service/ChatService.java
@@ -1,4 +1,20 @@
 package com.example.askchat.service;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChatService {
+
+    private final ChatModel chatModel;
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String message) {
+        return chatModel.call(message);
+    }
 }
